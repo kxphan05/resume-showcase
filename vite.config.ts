485f4a256023d191eab3b2c4f30a981d573205cb
pkx <kxphan05@gmail.com>
2026-02-13 14:37:43 +0800

    added vite folder:

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/resume-showcase/',
+  base: mode === 'production' ? '/resume-showcase/' : '/',
   server: {
     host: "::",
     port: 8080,
